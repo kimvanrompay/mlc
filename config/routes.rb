2f312d948a60b03affd7447f5a6eb3c8c189f77a
Ubuntu <ubuntu@ip-172-31-6-61.eu-west-3.compute.internal>
2023-01-21 13:41:35 +0000

    aanpassingen

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'nl/overons'
   get 'nl/diensten'
   get 'nl/contact'
+  get 'nl/vacature_student'  
   get 'nl/vacatures'
   get 'nl/legal'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
